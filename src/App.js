d6c9859d8367a4150778c250ecbf4155b8bd1a25
KAHO <84381486+michimichi2021@users.noreply.github.com>
2022-05-22 23:41:11 +0900

    ネストされたページの遷移

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
-import { Page1DetailA } from "Page1DetailA";
-import { Page1DetailB } from "Page1DetailB";
 import { Page2 } from "./Page2";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import "./styles.css";
 
 export default function App() {
@@ -23,15 +23,15 @@ export default function App() {
         </Route>
         <Route
           path="/page1"
-          render={() => (
+          render={({ match: { url } }) => (
             <Switch>
-              <Route exact path="/page1">
+              <Route exact path={url}>
                 <Page1 />
               </Route>
-              <Route path="/page1/detailA">
+              <Route path={`${url}/detailA`}>
                 <Page1DetailA />
               </Route>
-              <Route path="/page1/detailB">
+              <Route path={`${url}/detailB`}>
                 <Page1DetailB />
               </Route>
             </Switch>
